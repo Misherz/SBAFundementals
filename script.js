9eb9909b1ed6ed0d059290eb2563ffdfcf9d0059
Misherz <michelle.j.lugo@gmail.com>
2024-09-16 14:35:00 -0400

    initializing project

---
+++ script.js
@@ -0,0 +1,14 @@
+// Requirements
+//     Using let/const
+//     Using operators to perform calculations on variable and literals
+//     Using strings numbers boolean values within variables
+//     2 if/else statements
+//     use try/catch
+//     2 diff loops
+//         break || continue
+//     manipulate array && objects 
+//     functions for repeated tasks
+//     out process as shown
+//     no errors
+//     commit
+//     readme file
